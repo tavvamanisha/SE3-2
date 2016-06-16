0cc08b05c33d24b0d9ea00455ae14fa16199eafc
manishatavva <manisha.tavva@eagles.oc.edu>
2016-06-16 13:24:59 -0500

    university commit

---
+++ src/University.java
@@ -0,0 +1,18 @@
+
+public class University {
+	private String universityName;
+	private String universityAbb;
+	public String getUniversityName() {
+		return universityName;
+	}
+	public void setUniversityName(String universityName) {
+		this.universityName = universityName;
+	}
+	public String getUniversityAbb() {
+		return universityAbb;
+	}
+	public void setUniversityAbb(String universityAbb) {
+		this.universityAbb = universityAbb;
+	}
+	
+}
