de7ddd1a9389b98020bfd1d4e808cfdad9bda675
manishatavva <manisha.tavva@eagles.oc.edu>
2016-06-16 13:27:28 -0500

    grad school commit

---
+++ src/GradSchools.java
@@ -0,0 +1,22 @@
+
+public class GradSchools {
+	
+	private String schoolName;
+	private String schoolAbbr;
+	
+	public String getSchoolName() {
+		return schoolName;
+	}
+	public void setSchoolName(String schoolName) {
+		this.schoolName = schoolName;
+	}
+	public String getSchoolAbbr() {
+		return schoolAbbr;
+	}
+	public void setSchoolAbbr(String schoolAbbr) {
+		this.schoolAbbr = schoolAbbr;
+	}
+	
+	
+
+}
